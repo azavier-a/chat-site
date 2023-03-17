7f908e7dbecd04dff384a3ae57e88294c11b349c
azavier <avzaviercallen@gmail.com>
2023-03-17 15:35:50 -0400

    s

--- index.js
+++ index.js
@@ -6,20 +6,12 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 
-let users = 0;
-const user = {};
-
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
   console.log('user connected (' + socket.id + ')');
-  users++;
-  if(!user[socket.id])
-    user[socket.id] = users;
-  
-  io.emit('guest number', socket.id, user[socket.id]);
 
   socket.on('chat message', (msg, sig) => {
     console.log(sig + ' sent message: ' + msg);
@@ -27,7 +19,7 @@ io.on('connection', (socket) => {
   });
 
   socket.on('disconnect', () => {
-    console.log('user disconnected');
+    console.log('user disconnected (' + socket.id + ')');
   });
 });
 
