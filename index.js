1da2d3b788249e9b7c68b5f3bf32833191c2eba1
azavier <avzaviercallen@gmail.com>
2023-03-17 14:53:15 -0400

    Signatures

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 /*    SERVER SIDE CODE    */
 
-const expr = require('express');
-const http = require('http');
-const { Server } = require('socket.io');
+const express = require('express');
 
-const app = expr();
-const srvr = http.createServer(app);
-const io = new Server(srvr);
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', (socket) => {
-  //console.log('user connected');
+  console.log('user connected');
 
   socket.on('chat message', (msg) => {
-    //console.log('user sent message: ' + msg);
+    console.log('user sent message: ' + msg);
     io.emit('chat message', msg);
   });
 
   socket.on('disconnect', () => {
-    //console.log('user disconnected');
+    console.log('user disconnected');
   });
 });
 
-srvr.listen(3000, () => {
-  //console.log('listening on *:3000');
+server.listen(3000, () => {
+  console.log('listening on *:3000');
 });
